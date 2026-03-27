a3e11963ee5bcda1ceb66bfbb448accccbe41d02
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-03-27 10:07:28 +0900

    Update train_tokenizer.sh

--- train_tokenizer.sh
+++ train_tokenizer.sh
@@ -33,7 +33,7 @@ run_train () {
             --cfg_file "$CFG_FILE" \
             --num_bytes "$DATA_BYTES" \
             --vocab_size "$VOCAB_SIZE" \
-            --regex_string "[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]*[\p{Ll}\p{Lm}\p{Lo}\p{M}]+|[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]+[\p{Ll}\p{Lm}\p{Lo}\p{M}]*|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n/]*|\s*[\r\n]+|\s+(?!\S)|\s+"
+            --regex_string "(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]*[\p{Ll}\p{Lm}\p{Lo}\p{M}]+|[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]+[\p{Ll}\p{Lm}\p{Lo}\p{M}]*|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n/]?|\s*[\r\n]|\s+(?!\S)|\s+"
 
         python3 -m train.preprocess \
             --process_tgt "$OUTPUT_DIR" \
