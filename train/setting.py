9835db4da9ffe0880c1b6501990fbb2a2a8d5634
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-03-30 23:05:13 +0900

    Update setting.py

--- train/setting.py
+++ train/setting.py
@@ -28,7 +28,7 @@ def tokenize_korean(text):
 
         buffer = ""
         for morph, pos in morphs:
-            if pos.startswith(('J', 'E', 'X')):
+            if pos.startswith("J"):
                 if buffer:
                     tokens.append(buffer)
                     buffer = ""
@@ -61,27 +61,9 @@ def tokenized_corpus(text_files):
         
         yield "\n".join(texts).strip()
 
-def build_initial_alphabet():
-    alphabet = set()
-
-    alphabet.update(chr(c) for c in range(0xAC00, 0xD7A4)) # Hangul Syllables
-    alphabet.update(chr(c) for c in range(0x3131, 0x3164)) # Hangul Compatibility Jamo
-    
-    # Hanja
-    with open(BASE_DIR / "../data/hanja_level1.txt", "r", encoding="utf-8") as f:
-        alphabet.update(line.strip() for line in f if line.strip())
-
-    # Kanji
-    with open(BASE_DIR / "../data/kanji.txt", "r", encoding="utf-8") as f:
-        alphabet.update(line.strip() for line in f if line.strip())
-
-    alphabet.update(chr(c) for c in range(0x3041, 0x3097)) # Hiragana
-    alphabet.update(chr(c) for c in range(0x30A1, 0x30FB)) # Katakana
-
-    return sorted(alphabet)
-
 def build_special_tokens():
     special_tokens = [
+        "<unk>", # unk_token
         "<|startoftext|>", # bos_token
         "<|return|>", # eos_token
         "<|endoftext|>", # pad_token
@@ -90,74 +72,103 @@ def build_special_tokens():
     special_tokens += [" " * i for i in range(31, 1, -1)]
     special_tokens += ["\t" * i for i in range(9, 1, -1)]
     special_tokens += ["\n" * i for i in range(9, 1, -1)]
-
+    
     return special_tokens
 
+def build_initial_alphabet():
+    alphabet = set()
+
+    alphabet.update(chr(c) for c in range(0xAC00, 0xD7A4)) # Hangul Syllables
+    alphabet.update(chr(c) for c in range(0x3131, 0x3164)) # Hangul Compatibility Jamo
+
+    # Hanja
+    with open(BASE_DIR / "../data/hanja_level1.txt", "r", encoding="utf-8") as f:
+        alphabet.update(line.strip() for line in f if line.strip())
+
+    # Kanji
+    with open(BASE_DIR / "../data/kanji.txt", "r", encoding="utf-8") as f:
+        alphabet.update(line.strip() for line in f if line.strip())
+
+    # Hiragana
+    alphabet.update(chr(c) for c in range(0x3041, 0x3094))
+
+    # Katakana
+    alphabet.update(chr(c) for c in range(0x30A1, 0x30FA))
+
+    return sorted(alphabet)
+
 def train_tokenizer(text_files: list[str], vocab_size: int = 100000, regex_string: str = None):
-    tokenizer = Tokenizer(BPE())
-    trainer = BpeTrainer(
-        vocab_size=vocab_size,
-        show_progress=True,
-        special_tokens=build_special_tokens(), # FIXME
-        initial_alphabet=build_initial_alphabet(), # FIXME
+    tokenizer = Tokenizer(
+        BPE(
+            unk_token="<unk>",
+            byte_fallback=True,
+        )
     )
 
-    tokenizer.normalizer = normalizers.NFC()
+    tokenizer.normalizer = normalizers.Sequence([
+        normalizers.NFC(), 
+    ])
     
-    pretokenizers = [
+    tokenizer.pre_tokenizer = pre_tokenizers.Sequence([
         pre_tokenizers.Split(
             pattern=Regex(regex_string),
             behavior="isolated",
             invert=False,
         ),
-        # FIXME
+        # FIXME: For tokenize_korean()
         pre_tokenizers.Split(
             pattern=Regex("▁"),
             behavior="removed",
             invert=False,
         ),
-        pre_tokenizers.ByteLevel(
-            add_prefix_space=False,
-            trim_offsets=True,
-            use_regex=False,
-        ),
-    ]
-    tokenizer.pre_tokenizer = pre_tokenizers.Sequence(pretokenizers)
-    tokenizer.post_processor = processors.ByteLevel(add_prefix_space=True, trim_offsets=False, use_regex=True)
-    tokenizer.decoder = decoders.ByteLevel(add_prefix_space=True, trim_offsets=True, use_regex=True)
+    ])
+    
+    tokenizer.decoder = decoders.Sequence([
+        decoders.ByteFallback(),
+    ])
 
-    # FIXME
+    trainer = BpeTrainer(
+        vocab_size=vocab_size,
+        show_progress=True,
+        special_tokens=build_special_tokens() + [f"<0x{i:02X}>" for i in range(256)],
+        initial_alphabet=build_initial_alphabet(),
+    )
+
     iterator = tokenized_corpus(text_files)
     tokenizer.train_from_iterator(iterator, trainer)
 
     return tokenizer
 
 def extend_tokenizer(text_files: list[str], vocab_size: int = 100000, regex_string: str = None):
-    tokenizer = Tokenizer(BPE())
-    trainer = BpeTrainer(
-        vocab_size=vocab_size,
-        show_progress=True,
-        special_tokens=build_special_tokens(), # FIXME
-        initial_alphabet=build_initial_alphabet(), # FIXME
+    tokenizer = Tokenizer(
+        BPE(
+            unk_token="<unk>",
+            byte_fallback=True,
+        )
     )
 
-    tokenizer.normalizer = normalizers.NFC()
+    tokenizer.normalizer = normalizers.Sequence([
+        normalizers.NFC(), 
+    ])
     
-    pretokenizers = [
+    tokenizer.pre_tokenizer = pre_tokenizers.Sequence([
         pre_tokenizers.Split(
             pattern=Regex(regex_string),
             behavior="isolated",
             invert=False,
         ),
-        pre_tokenizers.ByteLevel(
-            add_prefix_space=False,
-            trim_offsets=True,
-            use_regex=False,
-        ),
-    ]
-    tokenizer.pre_tokenizer = pre_tokenizers.Sequence(pretokenizers)
-    tokenizer.post_processor = processors.ByteLevel(add_prefix_space=True, trim_offsets=False, use_regex=True)
-    tokenizer.decoder = decoders.ByteLevel(add_prefix_space=True, trim_offsets=True, use_regex=True)
+    ])
+    
+    tokenizer.decoder = decoders.Sequence([
+        decoders.ByteFallback(),
+    ])
+
+    trainer = BpeTrainer(
+        vocab_size=vocab_size,
+        show_progress=True,
+        special_tokens=build_special_tokens() + [f"<0x{i:02X}>" for i in range(256)],
+        initial_alphabet=build_initial_alphabet(),
+    )
 
     tokenizer.train(text_files, trainer)
 
